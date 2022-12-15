9c0eb7a1678b62e924ee9af75f560bc35b5d555d
Stakljar <drazenantunovic012@gmail.com>
2022-12-15 01:51:40 +0100

    Tests added

---
+++ RUAP_LV4/DifferentNewPasswordsTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace RUAP_LV4
+{
+    [TestFixture]
+    public class DifferentNewPasswordsTest
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void DifferentNewPasswordsTestCaseTest()
+        {
+            driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/");
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("example525328@gmail.com");
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("password");
+            driver.FindElement(By.XPath("//input[@value='Log in']")).Click();
+            driver.FindElement(By.LinkText("example525328@gmail.com")).Click();
+            driver.FindElement(By.LinkText("Change password")).Click();
+            driver.FindElement(By.Id("OldPassword")).Click();
+            driver.FindElement(By.Id("OldPassword")).Clear();
+            driver.FindElement(By.Id("OldPassword")).SendKeys("password");
+            driver.FindElement(By.Id("NewPassword")).Click();
+            driver.FindElement(By.Id("NewPassword")).Clear();
+            driver.FindElement(By.Id("NewPassword")).SendKeys("password12");
+            driver.FindElement(By.Id("ConfirmNewPassword")).Click();
+            driver.FindElement(By.Id("ConfirmNewPassword")).Clear();
+            driver.FindElement(By.Id("ConfirmNewPassword")).SendKeys("password13");
+            driver.FindElement(By.XPath("//input[@value='Change password']")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
